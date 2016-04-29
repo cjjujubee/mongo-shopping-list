6b539863239e16a042be4cf7408235d22e76dadf
cjjujubee <connie.jew@gmail.com>
2016-04-29 12:10:46 -0700

    Adds non-working Delete route

---
+++ services/item.js
@@ -0,0 +1,26 @@
+var Item = require('../models/item');
+
+exports.save = function(name, callback, errback) {
+    Item.create({ name: name }, function(err, item) {
+        if (err) {
+            errback(err);
+            return;
+        }
+        callback(item);
+    });
+};
+
+exports.list = function(callback, errback) {
+    Item.find(function(err, items) {
+        if (err) {
+            errback(err);
+            return;
+        }
+        callback(items);
+    });
+};
+
+exports.edit = function(callback, errback) {
+    var query = { $in: "name"};
+    Item.findOneAndUpdate(query, )
+};
