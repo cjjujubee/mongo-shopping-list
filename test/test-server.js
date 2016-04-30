2241eee5ceb5747438a0bc12bad6fe67e5d2e06e
cjjujubee <connie.jew@gmail.com>
2016-04-29 22:52:56 -0700

    Adds testing for editing and deleting items

--- test/test-server.js
+++ test/test-server.js
@@ -38,17 +38,6 @@ describe('Shopping List', function() {
                 done();
             });
     });
-    // it('should list a items on get', function(done) {
-    //     chai.request(app)
-    //         .get('/items/' + 1)
-    //         .end(function(err, res) {
-    //             should.equal(err, null);
-    //             res.should.have.status(200);
-    //             res.should.be.json;
-    //             res.body.name.should.be.equal('Tomatoes');
-    //             done();
-    //         });
-    // });
     it('should add an item on POST', function(done) {
         chai.request(app)
             .post('/items')
@@ -63,62 +52,93 @@ describe('Shopping List', function() {
                 res.body.name.should.be.a('string');
                 res.body._id.should.be.a('string');
                 res.body.name.should.equal('Kale');
-                console.log('HI', Item);
-                Item.list.should.be.a('array');
+                
+                Item.find({}, function(err,docs) {
+                    should.equal(err, null);
 
-                storage.items.should.have.length(4);
-                storage.items[3].should.be.a('object');
-                storage.items[3].should.have.property('_id');
-                storage.items[3].should.have.property('name');
-                storage.items[3].id.should.be.a('string');
-                storage.items[3].name.should.be.a('string');
-                storage.items[3].name.should.equal('Kale');
-                done();
+                    var list = docs;
+                    list.should.be.a('array');
+                    list.should.have.length(4);
+                    list[3].should.be.a('object');
+                    list[3].should.have.property('_id');
+                    list[3].should.have.property('name');
+                    list[3].id.should.be.a('string');
+                    list[3].name.should.be.a('string');
+                    list[3].name.should.equal('Kale');
+                    done();
+                });
             });
     });
-    // it('should edit an item on put', function(done) {
-    //     var itemEdit = {name: 'Apples'};
-    //     chai.request(app)
-    //         .put('/items/' + 1)
-    //         .send(itemEdit)
-    //         .end(function(err, res) {
-    //             should.equal(err, null);
-    //             res.should.have.status(200);
-    //             res.should.be.json;
-    //             storage.items[1].name.should.be.a('string');
-    //             storage.items[1].name.should.equal('Apples');
-    //             storage.items[1].should.be.a('object');
-    //             storage.items[1].should.have.property('id');
-    //             storage.items.should.have.length(4);
-    //             done();
-    //         });
+    it('should edit an item on put', function(done) {
+        var itemEdit = {name: 'Apples'};
+        
+        Item.find({}, function(err,docs) {
+            should.equal(err, null);
+            var itemId = docs[0]._id;
+            
+            chai.request(app)
+                .put('/items/' + itemId)
+                .send(itemEdit)
+                .end(function(err, res) {
+                    should.equal(err, null);
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    
+                    Item.find({_id: itemId}, function(err,docs) {
+                        should.equal(err, null);
 
-    // });
-    // it('should delete an item on delete', function(done) {
-    //     chai.request(app)
-    //         .delete('/items/' + 2)
-    //         .end(function(err, res) {
-    //             should.equal(err, null);
-    //             res.should.have.status(200);
-    //             res.should.be.json;
-    //             storage.items[2].name.should.be.equal('Kale');
-    //             storage.items[0].name.should.be.equal('Broad beans');
-    //             storage.items.should.have.length(3);
-    //             done();
+                        var list = docs;
+                        list[0].should.be.a('object');
+                        list[0].should.have.property('_id');
+                        list[0].should.have.property('name');
+                        list[0].name.should.be.a('string');
+                        list[0].name.should.equal('Apples');
+                        done();
+                    });                                  
+            });
+        });
+    });
+    it('should delete an item on delete', function(done) {
+        
+        Item.find({}, function(err,docs) {
+            should.equal(err, null);
+            var itemId = docs[0]._id;
+            
+            chai.request(app)
+                .delete('/items/' + itemId)
+                .end(function(err, res) {
+                    should.equal(err, null);
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    
+                    Item.find({_id: itemId}, function(err,docs) {
+                        should.equal(err, null);
 
-    //         });
-    // });
-    // it('should not get deleted item', function(done) {
-    //     chai.request(app)
-    //         .get('/items/' + 2)
-    //         .end(function(err, res) {
-    //             should.equal(err, null);
-    //             res.should.have.status(200);
-    //             res.should.be.json;
-    //             storage.items[2].name.should.be.equal('Peppers');
-    //             done();
-    //         });
-    // });
+                        var list = docs;
+                        console.log('YOOOOO', list);
+                        list.should.be.a('array');
+                        list.should.have.length(0);
+                        done();
+                    });                                  
+            });
+        });
+
+
+
+
+        // chai.request(app)
+        //     .delete('/items/' + 2)
+        //     .end(function(err, res) {
+        //         should.equal(err, null);
+        //         res.should.have.status(200);
+        //         res.should.be.json;
+        //         storage.items[2].name.should.be.equal('Kale');
+        //         storage.items[0].name.should.be.equal('Broad beans');
+        //         storage.items.should.have.length(3);
+        //         done();
+
+        //     });
+    });
 
     after(function(done) {
         Item.remove(function() {
@@ -126,107 +146,3 @@ describe('Shopping List', function() {
         });
     });
 });
-
-// describe('Shopping List', function() {
-
-
-//     it('should list items on get', function(done) {
-//         chai.request(app)
-//             .get('/items')
-//             .end(function(err, res) {
-//                 should.equal(err, null);
-//                 res.should.have.status(200);
-//                 res.should.be.json;
-//                 res.body.should.be.a('array');
-//                 res.body.should.have.length(3);
-//                 res.body[0].should.be.a('object');
-//                 res.body[0].should.have.property('id');
-//                 res.body[0].should.have.property('name');
-//                 res.body[0].id.should.be.a('number');
-//                 res.body[0].name.should.be.a('string');
-//                 res.body[0].name.should.equal('Broad beans');
-//                 res.body[1].name.should.equal('Tomatoes');
-//                 res.body[2].name.should.equal('Peppers');
-//                 done();
-//             });
-//     });
-//     it('should list a items on get', function(done) {
-//         chai.request(app)
-//             .get('/items/' + 1)
-//             .end(function(err, res) {
-//                 should.equal(err, null);
-//                 res.should.have.status(200);
-//                 res.should.be.json;
-//                 res.body.name.should.be.equal('Tomatoes');
-//                 done();
-//             });
-//     });
-//     it('should add an item on POST', function(done) {
-//         chai.request(app)
-//             .post('/items')
-//             .send({'name': 'Kale'})
-//             .end(function(err, res) {
-//                 should.equal(err, null);
-//                 res.should.have.status(201);
-//                 res.should.be.json;
-//                 res.body.should.be.a('object');
-//                 res.body.should.have.property('name');
-//                 res.body.should.have.property('id');
-//                 res.body.name.should.be.a('string');
-//                 res.body.id.should.be.a('number');
-//                 res.body.name.should.equal('Kale');
-//                 storage.items.should.be.a('array');
-//                 storage.items.should.have.length(4);
-//                 storage.items[3].should.be.a('object');
-//                 storage.items[3].should.have.property('id');
-//                 storage.items[3].should.have.property('name');
-//                 storage.items[3].id.should.be.a('number');
-//                 storage.items[3].name.should.be.a('string');
-//                 storage.items[3].name.should.equal('Kale');
-//                 done();
-//             });
-//     });
-//     it('should edit an item on put', function(done) {
-//         var itemEdit = {name: 'Apples'};
-//         chai.request(app)
-//             .put('/items/' + 1)
-//             .send(itemEdit)
-//             .end(function(err, res) {
-//                 should.equal(err, null);
-//                 res.should.have.status(200);
-//                 res.should.be.json;
-//                 storage.items[1].name.should.be.a('string');
-//                 storage.items[1].name.should.equal('Apples');
-//                 storage.items[1].should.be.a('object');
-//                 storage.items[1].should.have.property('id');
-//                 storage.items.should.have.length(4);
-//                 done();
-//             });
-
-//     });
-//     it('should delete an item on delete', function(done) {
-//         chai.request(app)
-//             .delete('/items/' + 2)
-//             .end(function(err, res) {
-//                 should.equal(err, null);
-//                 res.should.have.status(200);
-//                 res.should.be.json;
-//                 storage.items[2].name.should.be.equal('Kale');
-//                 storage.items[0].name.should.be.equal('Broad beans');
-//                 storage.items.should.have.length(3);
-//                 done();
-
-//             });
-//     });
-//     it('should not get deleted item', function(done) {
-//         chai.request(app)
-//             .get('/items/' + 2)
-//             .end(function(err, res) {
-//                 should.equal(err, null);
-//                 res.should.have.status(200);
-//                 res.should.be.json;
-//                 storage.items[2].name.should.be.equal('Peppers');
-//                 done();
-//             });
-//     });
-// });
